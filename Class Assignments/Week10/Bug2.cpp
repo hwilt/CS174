5324fa84dba1b793f05cff19c288f0b64920050e
hwilt <henrywwilt@gmail.com>
2020-11-13 14:24:55 -0500

    Update Bug2.cpp

--- Class Assignments/Week10/Bug2.cpp
+++ Class Assignments/Week10/Bug2.cpp
@@ -14,7 +14,7 @@ int* filterMultiples(int* arr, int N, int* NOut, int num) {
     *NOut = 0;
     for (int i = 0; i < N; i++) {
         if (arr[i] >= num && arr[i]%num == 0) {
-            *NOut++;
+            (*NOut)++;
         }
     }
     int* multiples = new int[*NOut];
